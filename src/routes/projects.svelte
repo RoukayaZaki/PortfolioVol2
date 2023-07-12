825058766c50e9073e515ed818d7c8e018e620c4
RoukayaZaki <roka.zaki2002@gmail.com>
2023-07-12 17:33:17 +0300

    Move images to lib

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
 	import ProjectItem from './ProjectItem.svelte';
 	import { base } from '$app/paths';
-	import cv from './../lib/RoukayaMohammed.pdf';
+	import cv from '$lib/RoukayaMohammed.pdf';
+	import img1 from '$lib/images/project-img1.png';
+	import img2 from '$lib/images/project-img2.png';
+	import img3 from '$lib/images/project-img3.png';
+	import img4 from '$lib/images/project-img4.jpg';
+
 	const projects = [
 		{
 			link: 'https://github.com/InnoSWP/BS21-08-CV-Parser',
-			img: `${base}/project-img1.png`,
+			img: img1,
 			alt: 'Project 1',
 			title: 'CV Parser',
 			description: 'A web application to parse CVs and extract info.'
 		},
 		{
 			link: 'https://github.com/RoukayaZaki/ChuckJokes',
-			img: `${base}/project-img2.png`,
+			img: img2,
 			alt: 'Project 2',
 			title: 'Chuck Norris',
 			description: "A flutter application displaying Chuck Norris' jokes."
 		},
 		{
 			link: 'https://github.com/MostafaKhaled2017/Flutter_Course_iExtract',
-			img: `${base}/project-img3.png`,
+			img: img3,
 			alt: 'Project 3',
 			title: 'Fix My English',
 			description: 'A flutter application to highlight writing style mistakes.'
 		},
 		{
 			link: 'https://github.com/RoukayaZaki/Accompaniment-Generation',
-			img: `${base}/project-img4.png`,
+			img: img4,
 			alt: 'Project 4',
 			title: 'Accompaniment Generation',
 			description: 'An evolutionary algorithm to generate accompaniment to a given melody.'
