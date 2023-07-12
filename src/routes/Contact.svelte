d7157b4a6644cd8b73c20100a8e5f7d1e1f25670
RoukayaZaki <roka.zaki2002@gmail.com>
2023-07-12 16:20:41 +0300

    fixing format

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,35 +1,40 @@
 <script lang="ts">
-    import { InstagramIcon, LinkedinIcon,  GithubIcon, SendIcon, MailIcon} from 'svelte-feather-icons';
+	import {
+		InstagramIcon,
+		LinkedinIcon,
+		GithubIcon,
+		SendIcon,
+		MailIcon
+	} from 'svelte-feather-icons';
 </script>
-  
+
 <section id="contact">
-    <h2>Contact Me</h2>
-    <ul class="contact-icons">
-      <li>
-        <a href="https://www.instagram.com/roukaya_zaki" target="_blank">
-          <InstagramIcon />
-        </a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/roukaya-mabrouk-a7094719b/" target="_blank">
-          <LinkedinIcon />
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/RoukayaZaki" target="_blank">
-          <GithubIcon />
-        </a>
-      </li>
-      <li>
-        <a href="https://telegram.me/GammaV" target="_blank">
-          <SendIcon />
-        </a>
-      </li>
-      <li>
-        <a href="mailto:roka.zaki2002@gmail.com" target="_blank">
-          <MailIcon />
-        </a>
-      </li>
-    </ul>
+	<h2>Contact Me</h2>
+	<ul class="contact-icons">
+		<li>
+			<a href="https://www.instagram.com/roukaya_zaki" target="_blank">
+				<InstagramIcon />
+			</a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/in/roukaya-mabrouk-a7094719b/" target="_blank">
+				<LinkedinIcon />
+			</a>
+		</li>
+		<li>
+			<a href="https://github.com/RoukayaZaki" target="_blank">
+				<GithubIcon />
+			</a>
+		</li>
+		<li>
+			<a href="https://telegram.me/GammaV" target="_blank">
+				<SendIcon />
+			</a>
+		</li>
+		<li>
+			<a href="mailto:roka.zaki2002@gmail.com" target="_blank">
+				<MailIcon />
+			</a>
+		</li>
+	</ul>
 </section>
-  
